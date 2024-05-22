5a174d911590c23c4099170f609380c8356e950c
ClasesParticularesUTN <ClasesParticularesFRSF@gmail.com>
2024-05-22 20:01:06 -0300

    Update horarios.js

--- horarios.js
+++ horarios.js
@@ -1,4 +1,4 @@
-var url = "https://script.google.com/macros/s/AKfycbyuUCwq_KN7PVeX1fmR_4K94R6YUVWFK2qCyQv0r9_oUj1lBDCmfsBRXdvNbQTMBQ/exec"; 
+var url = "https://script.google.com/macros/s/AKfycbwRgnygMwqKdBnUIRMndkJQ3OSdFJj7XtuIyyPW7LmZjEIhzgwbufRBqMSRTHr5yA/exec"; 
 var urlR = "https://script.google.com/macros/s/AKfycbwhOEx38Rh8e7zbcntvAANqGs1XI4dHOibVDqB04550Dpwh5O-n1ex6n2sS-640H8o/exec";
 let $siguiente = document.querySelector(".siguiente");
 let $botones = document.querySelectorAll(".caja");
