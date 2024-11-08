adf04ac1a816fb7710a5983738d392a129e1aa2f
ClasesParticularesUTN <ClasesParticularesFRSF@gmail.com>
2024-11-08 12:06:25 -0300

    Update enviarReserva.js

--- JS/enviarReserva.js
+++ JS/enviarReserva.js
@@ -1,6 +1,6 @@
-sinTurnosDisponibles = true;
+sinTurnosDisponibles = 0;
 document.querySelector(".botonReservar").addEventListener("click", async () => {
-    if(sinTurnosDisponibles){
+    if(sinTurnosDisponibles and numeroHoja == 0){
         Swal.fire({
             icon: "error",
             title: "Error",
